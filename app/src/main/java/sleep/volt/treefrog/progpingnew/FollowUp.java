e5a2687b48bfc0dbb1ea0db66b0e3e3b65babb90
David Gros <dgros@earthlink.net>
2015-09-26 22:23:47 -0500

    fix left out string

---
+++ app/src/main/java/sleep/volt/treefrog/progpingnew/FollowUp.java
@@ -0,0 +1,14 @@
+package sleep.volt.treefrog.progpingnew;
+
+import javax.xml.datatype.Duration;
+
+/**
+ * Created by dgros_000 on 9/26/2015.
+ */
+public class FollowUp {
+    public Duration when;
+    public String questionString;
+    public FollowUp(String questionName, Duration timeFromNow){
+
+    }
+}
---
+++ app/src/main/java/sleep/volt/treefrog/progpingnew/QuestionScheduler.java
@@ -0,0 +1,11 @@
+package sleep.volt.treefrog.progpingnew;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dgros_000 on 9/26/2015.
+ */
+public class QuestionScheduler {
+    ArrayList<FollowUp> followUps;
+
+}
